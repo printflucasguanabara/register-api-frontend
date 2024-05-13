e9ac71277139e1c56ebcd4b88bfd0a80812727e4
Lucas F. Guanabara <lucasff.guanabara@gmail.com>
2024-05-13 20:44:32 -0300

    api consume

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
+import { useEffect, useState } from 'react'
 import { FiTrash } from 'react-icons/fi'
+import { api } from './services/api'
+
+interface CustomerProps{
+  id:string;
+  name: string;
+  email: string;
+  status: boolean;
+  created_at: string;
+}
 
 export default function App(){
+
+  const[customers, setCustomers] = useState<CustomerProps[]>([])
+
+  useEffect(() => {
+    loadCustomers()
+  }, [])
+
+  async function loadCustomers(){
+    const response = await api.get("/customers")
+    setCustomers(response.data);
+  }
+
   return (
     <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
       <main className="my-10 w-full md:max-w-2xl">
@@ -20,18 +42,22 @@ export default function App(){
               className="cursor-pointer w-full p-2 bg-green-500 rounded font-medium" />
             </form>
 
-            <section className="flex flex-col">
-              <article
-              className="w--full bg-white rounded p-2 relative hover:scale-105 duration-200">
-                <p><span className="font-medium">Nome:</span> Lucas</p>
-                <p><span className="font-medium">E-Mail:</span> test@teste.com</p>
-                <p><span className="font-medium">Status:</span> ATIVO</p>
+            <section className="flex flex-col gap-4">
+
+              {customers.map((customer) => (
+                <article
+                key={customer.id}
+                className="w--full bg-white rounded p-2 relative hover:scale-105 duration-200">
+                <p><span className="font-medium">Nome:</span>{customer.name}</p>
+                <p><span className="font-medium">E-Mail:</span>{customer.email}</p>
+                <p><span className="font-medium">Status:</span>{customer.status ? "Ativo" : "INATIVO"}</p>
 
                 <button
                 className="bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2">
                     <FiTrash size={18} color="#FFF" />
                 </button>
               </article>
+              ))}
             </section>
 
       </main>
